62899991c458413945fa3537dd1de704157fe465
Alvaro <bfpdevel@gmail.com>
2026-04-14 09:38:23 -0300

    feat: add support for parsing and rendering inline style assets in S3 deployment components

--- models/Version.php
+++ models/Version.php
@@ -148,6 +148,21 @@ class Version extends Model
             $obDoc    = new Document($sHtml);
             $arAssets = [];
 
+            // Parse <style /> (inline styles, e.g. CSS @layer order declarations)
+            $arStyle = $obDoc->find('head > style');
+
+            if (!empty($arStyle) && is_array($arStyle)) {
+                foreach ($arStyle as $obStyle) {
+                    $sContent = $obStyle->text();
+
+                    if (!$sContent) {
+                        continue;
+                    }
+
+                    $arAssets[] = ['src' => null, 'type' => 'style', 'content' => $sContent];
+                }
+            }
+
             // Parse <script />
             $arScript = $obDoc->find('head > script');
 
@@ -161,7 +176,7 @@ class Version extends Model
                         continue;
                     }
 
-                    $arAssets[] = ['src' => $sSrc, 'attrs' => $arScriptAttr];
+                    $arAssets[] = ['src' => $sSrc, 'type' => 'script', 'attrs' => $arScriptAttr];
                 }
             }
 
@@ -185,7 +200,7 @@ class Version extends Model
                         continue;
                     }
 
-                    $arAssets[] = ['src' => $sHref, 'attrs' => $arLinkAttr];
+                    $arAssets[] = ['src' => $sHref, 'type' => 'link', 'attrs' => $arLinkAttr];
                 }
             }
 
